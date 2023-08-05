d17c0d1f26a0c38482148dd7b3821d177b999f2e
gabrielestefono <gabrielestefono@hotmail.com>
2023-08-05 15:01:43 -0300

    Recomeçando

--- client/components/header/Navbar/MadeIcon.jsx
+++
@@ -1,9 +0,0 @@
-import estilo from "./MadeIcon.module.scss";
-
-export default function MadeIcon(props){
-    return (
-        <div className={estilo.madeicon}>
-            <p>{props.texto}</p>
-        </div>
-    )
-}
